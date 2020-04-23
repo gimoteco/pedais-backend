63b6db6e04f5a85de96793ccb42cf87dbd1c8af0
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2020-04-23 17:14:36 -0400

    test: api tests w/ authenticated user

--- api/server.apiTest.ts
+++ api/server.apiTest.ts
@@ -1,22 +1,41 @@
 import ApolloClient, { gql } from "apollo-boost"
+import { Amplify, Auth } from "aws-amplify"
 import fetch from "node-fetch"
 import { setupInMemoryMongoDB } from "../infrastructure/databaseRepositories/setupInMemoryMongoDB"
 import Group from "../infrastructure/models/Group"
 import "./server"
+import awsExports from "./__testing/aws-exports"
 
-const client = new ApolloClient({
-    uri: "http://localhost:4000/graphql",
-    fetch: fetch as any,
-})
+(global as any).fetch = fetch
+
+Amplify.configure(awsExports)
+
+async function getClient(anonymous = true) {
+    const user = anonymous ? null : await Auth.signIn("teste@pedais.com.br", "teste123")
+
+    const client = new ApolloClient({
+        uri: "http://localhost:4000/graphql",
+        fetch: fetch as any,
+        headers: {
+            "Authorization": user ? `Bearer ${user.signInUserSession.idToken.jwtToken}` : null
+        }
+    })
+    return client
+}
 
 describe("GraphQL api", () => {
+    let anonymousClient
     setupInMemoryMongoDB()
+
+    beforeAll(async () => {
+        anonymousClient = await getClient()
+    })
 
     it("should fetch the groups", async () => {
         const groupName = "Chickens Bikers"
         await Group.create({ name: groupName })
 
-        const { data } = await client
+        const { data } = await anonymousClient
             .query({
                 query: gql`
               {
@@ -29,5 +48,21 @@ describe("GraphQL api", () => {
 
         expect(data.groups.length).toBe(1)
         expect(data.groups[0]).toMatchObject({ name: groupName })
+    })
+
+    it("should get user's parties", async () => {
+        const authenticatedClient = await getClient(false)
+
+        const { data } = await authenticatedClient.query({
+            query: gql`
+          {
+            myParties {
+              name
+            }
+          }
+        `
+        })
+
+        expect(data.myParties.length).toBe(0)
     })
 })
